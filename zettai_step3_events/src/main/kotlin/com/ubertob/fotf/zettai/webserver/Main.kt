2f80b8f4022cacbf6d7a92919224f98d37453a4d
Uberto Barbini <uberto.gama@gmail.com>
2023-06-17 08:38:24 +0900

    fixed the main fun example data (thanks to Yonatan Karp-Rudin)
    Signed-off-by: Uberto Barbini <uberto.gama@gmail.com>

--- zettai_step3_events/src/main/kotlin/com/ubertob/fotf/zettai/webserver/Main.kt
+++ zettai_step3_events/src/main/kotlin/com/ubertob/fotf/zettai/webserver/Main.kt
@@ -1,9 +1,9 @@
 package com.ubertob.fotf.zettai.webserver
 
+import com.ubertob.fotf.zettai.commands.AddToDoItem
+import com.ubertob.fotf.zettai.commands.CreateToDoList
 import com.ubertob.fotf.zettai.commands.ToDoListCommandHandler
 import com.ubertob.fotf.zettai.domain.*
-import com.ubertob.fotf.zettai.domain.ToDoStatus.Done
-import com.ubertob.fotf.zettai.domain.ToDoStatus.InProgress
 import com.ubertob.fotf.zettai.events.ToDoListEventStore
 import com.ubertob.fotf.zettai.events.ToDoListEventStreamerInMemory
 import org.http4k.server.Jetty
@@ -11,13 +11,16 @@ import org.http4k.server.asServer
 import java.time.LocalDate
 
 fun main() {
-    val fetcher = ToDoListFetcherFromMap(storeWithExampleData())
+    val fetcher = ToDoListFetcherFromMap(mutableMapOf())
     val streamer = ToDoListEventStreamerInMemory()
     val eventStore = ToDoListEventStore(streamer)
 
     val commandHandler =
         ToDoListCommandHandler(eventStore, fetcher)
+
     val hub = ToDoListHub(fetcher, commandHandler, eventStore)
+
+    hub.withExampleToDoList().withExampleItems()
 
     Zettai(hub).asServer(Jetty(8080)).start()
 
@@ -25,20 +28,25 @@ fun main() {
 
 }
 
-fun storeWithExampleData(): ToDoListStore = mutableMapOf(
-    User("uberto") to
-            mutableMapOf(exampleToDoList().listName to exampleToDoList())
-)
 
-private fun exampleToDoList(): ToDoList {
-    return ToDoList(
-        listName = ListName.fromTrusted("book"),
-        items = listOf(
-            ToDoItem("prepare the diagram", LocalDate.now().plusDays(1), Done),
-            ToDoItem("rewrite explanations", LocalDate.now().plusDays(2), InProgress),
-            ToDoItem("finish the chapter"),
-            ToDoItem("draft next chapter")
-        )
+private fun ToDoListHub.withExampleToDoList(): ToDoListHub =
+    also { handle(CreateToDoList(User("uberto"), ListName("book"))) }
+
+private fun ToDoListHub.withExampleItems(): ToDoListHub =
+    also { exampleItems.forEach { handle(it) } }
+
+private val exampleItems = sequence {
+    val user = User("uberto")
+    val listName = ListName("book")
+    yieldAll(
+        listOf(
+            AddToDoItem(user, listName, ToDoItem("prepare the diagram", tomorrow(), ToDoStatus.Done)),
+            AddToDoItem(user, listName, ToDoItem("rewrite explanations", dayAfterTomorrow(), ToDoStatus.InProgress)),
+            AddToDoItem(user, listName, ToDoItem("finish the chapter")),
+            AddToDoItem(user, listName, ToDoItem("draft next chapter")),
+        ),
     )
 }
 
+private fun tomorrow(): LocalDate = LocalDate.now().plusDays(1)
+private fun dayAfterTomorrow(): LocalDate = LocalDate.now().plusDays(2)
